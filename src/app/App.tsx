b591c803a687ebcf6a3dd3bff611e572a865791a
a1exssS <porkkoa@gmail.com>
2025-07-28 20:51:03 +0300

    move to FSD arc

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import './styles/index.scss';
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { ContactPageLazy } from './pages/ContactPage/ContactPage.lazy';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shered/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 const App = () => {
 
@@ -18,9 +17,8 @@ const App = () => {
          <Link to={'/about'}>О сайте</Link>
          <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-               <Route path={'/about'} element={<AboutPageAsync />} />
-               <Route path={'/'} element={<MainPageAsync />} />
-               <Route path={'/contact'} element={<ContactPageLazy />} />
+               <Route path={'/about'} element={<AboutPage />} />
+               <Route path={'/'} element={<MainPage />} />
             </Routes>
          </Suspense>
       </div>
