28d8ca2de2a6da87d90bbc686b5cebd5b427ad09
a1exssS <porkkoa@gmail.com>
2025-09-16 20:37:06 +0300

    fix infinite loading

--- src/shered/lib/hooks/useInfitniteScroll/useInfitniteScroll.ts
+++ src/shered/lib/hooks/useInfitniteScroll/useInfitniteScroll.ts
@@ -8,12 +8,14 @@ export interface useInfitniteScrollOptions {
 }
 export function useInfitniteScroll({ callback, triggerRef, wrapperRef }: useInfitniteScrollOptions) {
    useEffect(() => {
+      const wrapperElement = wrapperRef.current
+      const triggerElement = triggerRef.current
 
       let observer: IntersectionObserver | null = null
 
       if (callback) {
          const options = {
-            root: wrapperRef.current,
+            root: wrapperElement,
             rootMargin: "0px",
             scrollMargin: "0px",
             threshold: 1.0,
@@ -24,12 +26,11 @@ export function useInfitniteScroll({ callback, triggerRef, wrapperRef }: useInfi
                callback()
             }
          }, options);
-         observer.observe(triggerRef.current)
+         observer.observe(triggerElement)
 
          return () => {
-            if (observer) {
-               // eslint-disable-next-line
-               observer.unobserve(triggerRef.current)
+            if (observer && triggerElement) {
+               observer.unobserve(triggerElement)
             }
          }
 
